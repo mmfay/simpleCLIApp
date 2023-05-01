d698709fe147588afb5abed31ce006b7f95a0f7a
mmfay <mmfay3@gmail.com>
2023-05-01 04:11:16 -0700

    Add Options to App

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,25 @@
 #! /usr/bin/env node
+const utils = require('./utils.js')
+
 console.log("Hello World!");
 yargs = require("yargs");
-const usage = "\nUsage: tran <lang_name> sentence to be translated";const options = yargs  
+const usage = "\nUsage: mf <word> <-- to be kicked back";
+const options = yargs  
       .usage(usage)  
-      .option("l", {alias:"languages", describe: "List all supported languages.", type: "boolean", demandOption: false })                                                                                                
+      .option("k", {alias:"kickback", describe: "Kicks Back Word.", type: "boolean", demandOption: false })     
+      .option("m", {alias:"matthew", describe: "run test function.", type: "boolean", demandOption: false })                                                                                         
       .help(true)  
       .argv;
+if(yargs.argv.m == true || yargs.argv.matthew == true){  
+      utils.showTest();  
+      return;  
+}
+if(yargs.argv.kb == true || yargs.argv.kickback == true){  
+      utils.showTest();  
+      return;  
+}
+if(yargs.argv._[0] == null){  
+
+      utils.showHelp();  
+      return;  
+}
